c4f6f561dba727b99650fe2c785e264c03cb4f69
rldh1111 <rldh1111@github.com>
2022-10-11 12:35:58 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include "polonomial.h"
-#include "set"
-
+#include "conio.h"
 using namespace std;
-
 
 int main() {
 
-
-
-    // TODO 구현한 모든 함수 생성자 및 연산자 올바르게 작동하는지 확인하기 호출 전후 현재 다항식의 내용 표시
     polynomial p1;
-
     node *n1 = new node(-4.0, 0);
     node *n2 = new node(-4.0, 1);
     node *n3 = new node(-4.0, 2);
@@ -31,6 +25,7 @@ int main() {
     p2.list_insert(n7, 1);
     p2.list_insert(n6, 2);
     p2.list_insert(n5, 3);
+
     cout << "Data Structure " << endl;
     cout << "Student Id: 2019203058" << endl;
     cout << "name: Kwon gio" << endl;
@@ -52,14 +47,24 @@ int main() {
 
     for (int i = 0; i < p1.list_length(); ++i) {
         cout << "p1.list_locate(" << i << "): " << p1.list_locate(i) << endl;
-        cout << "p1.list_locate(" << i << "): " << p1.list_locate(i)->coefficients << endl;
-        cout << "p1.list_locate(" << i << "): "<< p1.list_locate(i)->exponents << endl;
+        cout << "p1.list_locate(" << i << ")->coefficients: " << p1.list_locate(i)->coefficients << endl;
+        cout << "p1.list_locate(" << i << ")->exponents: "<< p1.list_locate(i)->exponents << endl;
         cout << "" << endl;
     }
 
-    polynomial p3;
+    polynomial p3(p2);
+    polynomial p4(4.0);
+    cout << "polynomial p3(p2) copy constructor:  ";
+    p3.show_content();
+    cout << "" << endl;
+
+    cout << "polynomial p4(4.0):  ";
+    p4.show_content();
+    cout << "" <<endl;
+
     p3.list_copy(p1);
-    cout << "p3.list_copy:  ";
+
+    cout << "p3.list_copy(p1):  ";
     p3.show_content();
     cout << "" << endl;
 
@@ -90,7 +95,7 @@ int main() {
 
 
     cout << "p3 = p1 - p2: ";
-    p3 = p1 - p2;
+    p3 = (p1 - p2);
     p3.show_content();
 
 
@@ -106,11 +111,11 @@ int main() {
     p3 -= p1;
     p3.show_content();
 
-    cout << "p3 *=p1: ";
+    cout << "p3 *= p1: ";
     p3 *= p1;
     p3.show_content();
 
-
+    system("pause");
     return 0;
 
 }
